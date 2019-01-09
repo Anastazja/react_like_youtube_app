ffcb3b2836cb9ac9ba9ad75e50b656ed0ca300e6
Anastazja <anastasiia.rzeszow@gmail.com>
2019-01-09 11:15:50 +0100

    create youtube request

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../api/youtube'
 
 class App extends React.Component {
+
+    onTextSubmit = text => {
+        youtube.get('/search', {
+            params: {
+                q: text
+            }
+        });
+    };
+
     render() {
         return (
             <div className="ui container">
-                <SearchBar/>
+                <SearchBar onFormSubmit = {this.onTextSubmit}/>
             </div>
         );
     }
