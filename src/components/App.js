a260da559060d0b054a131af8fa53a594437c927
Anastazja <anastasiia.rzeszow@gmail.com>
2019-01-11 18:20:49 +0100

    show list of finded videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube'
+import VideoList from './VideoList';
 
 class App extends React.Component {
 
-    onTextSubmit = text => {
-        youtube.get('/search', {
+    state = {videos: []};
+
+    onTextSubmit = async text => {
+        const response = await youtube.get('/search', {
             params: {
                 q: text
             }
         });
+
+        console.log(response.data.items);
+        this.setState({videos: response.data.items});
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit = {this.onTextSubmit}/>
+                There is: {this.state.videos.length} results
+                <VideoList videos = {this.state.videos}/>
             </div>
         );
     }
